0b0842f61ad2bea696b856464bf0be12ab6d41c3
Maaz Munir <mmunir@hubspot.com>
2022-06-07 10:55:47 -0700

    prettier and auth start

--- components/Doodle.js
+++ components/Doodle.js
@@ -1,5 +1,5 @@
 import "css-doodle";
 
-export default function Doodle({ rule = "" }) {return(
-  <css-doodle click-to-update>{rule}</css-doodle>
-)};
+export default function Doodle({ rule = "" }) {
+  return <css-doodle click-to-update>{rule}</css-doodle>;
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,29 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
-
         ...initialProps,
         styles: (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossOrigin="true"
+            />
             <link
               href="https://fonts.googleapis.com/css2?family=Karla:wght@400;600;700&display=swap"
               rel="stylesheet"
@@ -29,9 +32,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
