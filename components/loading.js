126ba21fabfea78ed0096b39b41abd55f029671d
Maaz Munir <mmunir@hubspot.com>
2022-09-07 21:59:25 -0700

    test

--- components/loading.js
+++ components/loading.js
@@ -1,5 +1,5 @@
 import { Triangle } from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import styled from "styled-components";
 
 const LoadingWrapper = styled.div`
