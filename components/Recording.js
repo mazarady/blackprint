eb61655368ba4c68a8883f11a27cfed90bdef067
Maaz Munir <mmunir@hubspot.com>
2022-10-21 19:14:27 -0700

    updated

--- components/Recording.js
+++ components/Recording.js
@@ -3,28 +3,53 @@ import styled from "styled-components";
 import { useRef, useEffect } from "react";
 
 const StyledWrapper = styled.div`
-  max-width: 720px;
   height: auto;
   border-radius: 6px;
-  margin-bottom: 15px;
+  margin: 15px 0px;
+  display: grid;
+  grid-row-gap: 15px;
+  @media (min-width: 990px) {
+    grid-template-columns: minmax(375px, 720px) 210px;
+    grid-column-gap: 40px;
+  }
 `;
 
 const StyledTime = styled.small`
-    font-family: "Karla";
-    font-size: 15.5px;
-    line-height: 1;
-    cursor: pointer;
-    transition: all 500ms ease;
-    display: inline-block;
-    &::after {
-      content: '';
-      width: 100%;
-      height: 1px;
-      display: block;
-      background: black;
-      transition: 300ms;
-    }
-`
+  font-family: "Karla";
+  color: #433e40;
+  font-size: 15.5px;
+  line-height: 1;
+  cursor: pointer;
+  transition: all 500ms ease;
+  display: inline-block;
+  &::after {
+    content: "";
+    width: 100%;
+    height: 1px;
+    display: block;
+    background: #433e40;
+    transition: 300ms;
+  }
+`;
+
+const StyledTimeStampWrapper = styled.div`
+  display: grid;
+  grid-row-gap: 5px;
+  border-radius: inherit;
+  background: #E3ACB9;
+  padding: 25px;
+  @media (min-width: 990px) {
+  }
+`;
+
+const StyledChapter = styled(StyledTime)`
+  cursor: initial;
+  font-size: 16.5px;
+  font-weight: bold;
+  &::after {
+    height: 0px;
+  }
+`;
 
 export default function Recording(data) {
   let timeStampRefs;
@@ -39,9 +64,8 @@ export default function Recording(data) {
     time = time.trim().split(":");
     let seconds;
     if (time.length === 3) {
-      seconds = (+time[0]) * 60 * 60 + (+time[1]) * 60 + (+time[2]);
-    }
-    else {
+      seconds = +time[0] * 60 * 60 + +time[1] * 60 + +time[2];
+    } else {
       seconds = +time[0] * 60 + +time[1];
     }
     seconds.toString();
@@ -51,47 +75,61 @@ export default function Recording(data) {
   };
 
   const renderTimeStamps = (timestamps, key) => {
-    return (
-      timestamps.split(',').map((link, index) => {
-        const linkDesc = link.trim().split(' ');
-        return (
-          <div key={index}>
-            <StyledTime
-              data-time={linkDesc[0]}
-              onClick={(evt) => {
-                onClickHandler(evt, key);
-              }}
-            >
-              {linkDesc[0]}
-            </StyledTime>
-            <small
-              style={{
-                fontFamily: "Karla",
-                fontSize: "15.5px",
-              }}
-            >
-              {" " + linkDesc[1]}
-            </small>
-          </div>
-        );
-      })
-    )
-  }
+    return timestamps.split(",").map((link, index) => {
+      const linkDesc = link.trim().split(" ");
+      return (
+        <div key={index}>
+          <StyledTime
+            data-time={linkDesc[0]}
+            onClick={(evt) => {
+              onClickHandler(evt, key);
+            }}
+          >
+            {linkDesc[0]}
+          </StyledTime>
+          <small
+            style={{
+              fontFamily: "Karla",
+              fontSize: "15.5px",
+              color: "#433E40",
+            }}
+          >
+            {" " + linkDesc[1]}
+          </small>
+        </div>
+      );
+    });
+  };
 
-  const videos = data.data.map(({title, loom, timestamps}, key) => {
+  const videos = data.data.map(({ title, loom, timestamps }, key) => {
     return (
       <StyledWrapper key={key}>
         <div
           style={{
-            marginTop: "15px",
             width: "100%",
             height: "100%",
             borderRadius: "inherit",
+            lineHeight: "1",
           }}
         >
-          <h4 style={{margin: 0, paddingBottom: '5px', fontFamily: 'Karla', fontWeight: 'initial'}}>{title}</h4>
+          {title && (
+            <h4
+              style={{
+                margin: 0,
+                paddingBottom: "5px",
+                fontFamily: "Karla",
+                fontWeight: "initial",
+              }}
+            >
+              {title}
+            </h4>
+          )}
           <iframe
-            style={{ width: "100%", height: "450px", borderRadius: "inherit" }}
+            style={{
+              width: "100%",
+              height: "450px",
+              borderRadius: "inherit",
+            }}
             src={loom}
             frameBorder="true"
             ref={timeStampRefs.current[key]}
@@ -100,11 +138,15 @@ export default function Recording(data) {
             allowFullScreen
           ></iframe>
         </div>
-        {timestamps && renderTimeStamps(timestamps, key)}
+        {timestamps && (
+          <StyledTimeStampWrapper>
+            <StyledChapter>Chapters</StyledChapter>
+            {renderTimeStamps(timestamps, key)}
+          </StyledTimeStampWrapper>
+        )}
       </StyledWrapper>
     );
   });
-
 
   return videos;
 }
