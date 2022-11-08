a7bc6487fa2ccf507883babc6fd68b922ef2ca30
Maaz Munir <mmunir@hubspot.com>
2022-11-07 21:05:24 -0800

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,95 +1,45 @@
 import styled from "styled-components";
-import { H1, Sub, Small, H2, H5 } from "../components/Headers";
+import { H1, H5 } from "../components/Headers";
 import Head from "next/head";
+import How from "../components/How";
+import Questions from "../components/Questions";
+import Benefits from "../components/Benefits";
+import Button from "../components/Button";
+import Cohort from "../components/Cohort";
+import Footer from "../components/Footer";
+import BenefitsOne from "../components/BenefitsOne";
+import BenefitsTwo from "../components/BenefitsTwo";
+import BenefitsThree from "../components/BenefitsThree";
+import AnchorText from "../components/AnchorText";
 
-const HeroLayout = styled.section`
-  background-color: white;
+const HeroLayout = styled.div`
+  padding: 112px 64px;
   display: grid;
-  align-items: center;
-  justify-content: center;
-  background-image: url("/forest.webp");
-  background-repeat: no-repeat;
-  background-position: center bottom;
-  background-size: 290%;
-  @media (min-width: 376px) {
-    background-size: 350%;
-  }
-  @media (min-width: 500px) {
-    background-size: 300%;
-  }
-  @media (min-width: 576px) {
-    background-size: 220%;
-  }
-  @media (min-width: 768px) {
-    background-size: 175%;
-  }
-  @media (min-width: 920px) {
-    background-size: 160%;
-  }
-  @media (min-width: 1200px) {
-    background-size: 120%;
-  }
-  @media (min-width: 1400px) {
-    background-size: 100%;
-  }
 `;
 
 const TextWrapper = styled.div`
-  margin-bottom: 8rem;
-  text-align: right;
-  padding: 0px 20px;
-  display: grid;
-  grid-row-gap: 7px;
-  @media (min-width: 376px) {
-    margin-bottom: 18rem;
+  text-align: center;
+  h5 {
+    margin-top: 8px;
   }
-  @media (min-width: 576px) {
-    margin-bottom: 17rem;
+  button {
+    margin-top: 30px;
+    margin-bottom: 5px;
   }
 `;
 
-const StyledSection = styled.section`
-  display: grid;
-  grid-template-columns: 616px 616px;
-  grid-column-gap: 80px;
-  justify-content: center;
-  padding: 110px 64px;
-`;
-
-const StyledSteps = styled.ul`
-  margin: 0;
-  padding: 0;
-  li {
-    padding: 0;
-    margin: 0;
-    list-style-type: none;
-    padding-bottom: 78px;
-    &:not(:first-of-type) {
-      margin-top: 20px;
-    }
-    &.union {
-      background: url("/union.png") no-repeat left top;
-      padding-left: 90px;
-    }
-    &.vector {
-      background: url("/vector.png") no-repeat left top;
-      padding-left: 90px;
-    }
-    h5 {
-      padding-bottom: 10px;
-    }
-  }
-`;
-
-const StyledLeft = styled.div`
-  p {
-    padding-bottom: 16px;
-  }
+const ImageWrapper = styled.div`
+  background: url("/board.gif") no-repeat center center;
+  width: 100%;
+  height: 640px;
+  margin: auto;
+  border-radius: 16px;
+  margin-top: 20px;
 `;
 
 const Home = () => {
   return (
-    <div>
+    <section>
       <HeroLayout className="modal">
         <Head>
           <title>Home</title>
@@ -99,49 +49,24 @@ const Home = () => {
           />
         </Head>
         <TextWrapper>
-          <H1>Introducing Blackprint</H1>
-          <Sub>Learn to code from professionals.</Sub>
+          <H1>Blackprint</H1>
+          <H5>
+            Learn to code, in as little as 8 months, from industry
+            professionals.
+          </H5>
+          <AnchorText text="Learn More" id="learn-more" />
         </TextWrapper>
+        <ImageWrapper />
       </HeroLayout>
-      <StyledSection>
-        <StyledLeft>
-          <Small>How it works</Small>
-          <H2>
-            Headline with USP related to how your product or service works
-          </H2>
-        </StyledLeft>
-        <StyledSteps>
-          <li className="union">
-            <H5>Short summary of step one</H5>
-            <Small>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique.
-            </Small>
-          </li>
-          <li className="union">
-            <H5>Short summary of step one</H5>
-            <Small>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique.
-            </Small>
-          </li>
-          <li className="union">
-            <H5>Short summary of step one</H5>
-            <Small>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique.
-            </Small>
-          </li>
-          <li className="vector">
-            <H5>Short summary of step one</H5>
-            <Small>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse varius enim in eros elementum tristique.
-            </Small>
-          </li>
-        </StyledSteps>
-      </StyledSection>
-    </div>
+      <How />
+      <Benefits />
+      <BenefitsOne />
+      <BenefitsTwo />
+      <BenefitsThree />
+      <Cohort />
+      <Questions />
+      <Footer />
+    </section>
   );
 };
 
